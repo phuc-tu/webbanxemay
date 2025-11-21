3d3c3b37a302daadbe6df77960b0658842cdb972
unknown <nguyenphuctu.0904@gmail.com>
2025-11-21 09:38:25 +0700

    Initial commit - Website ban xe may

---
+++ pages/footer.php
@@ -0,0 +1,40 @@
+<div class="clear"></div>
+<footer class="footer">
+      <div class="footer_image">
+        <img src="images/logo_style3.png" alt="Logo" />
+      </div>
+
+      <div class="footer_main">
+        <div class="footer_title">
+          <p>3TH - Motobikes Shop</p>
+        </div>
+
+        <div class="footer_category">
+          <a href="index.php">Trang chủ</a>
+          <a href="index.php">Sản phẩm</a>
+          <a href="index.php?quanly=tintuc">Tin tức</a>
+          <a href="index.php?quanly=lienhe">Liên hệ</a>
+        </div>
+        <div class="footer_ social">
+          <!-- <a href="#Phone"><i class="fa-solid fa-phone"></i></a> -->
+          <!-- <a href="#Gmail"><i class="fa-solid fa-envelope"></i></a> -->
+          <a href="https://www.facebook.com/"><i class="fa-brands fa-square-facebook"></i></a>
+          <a href="#Instagram"><i class="fa-brands fa-instagram"></i></a>
+        </div>
+
+        <div class="footer_contact">
+          <p>Điện thoại: +84 123456789</p>
+          <p>Gmail: doubletshop@gmail.com</p>
+          <p>
+            Địa chỉ: 273 An Vương Dương Vương-Thành Phố Hồ Chí Minh
+          </p>
+        </div>
+        <hr />
+        <div class="footer_copyright">
+          <p>
+            Copyright ©2025 All rights reserved | 
+            <b>Double T</b>
+          </p>
+        </div>
+      </div>
+</footer>
